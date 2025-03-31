2827df8c5fe5aafcce75232dcb7ca26a22411fcb
tanzs <13640282804@163.com>
2025-03-31 18:12:39 +0800

    feat: Add react-native-progress and related dependencies

--- src/app/TodayTask/Check.tsx
+++ src/app/TodayTask/Check.tsx
@@ -4,13 +4,23 @@ import Pictrue from '../../components/Pictrue';
 import Drawer from "../../components/Drawer";
 import Message from '../../components/Message';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as Progress from 'react-native-progress';
+
 
 const Check = () => {
     return (
         <GestureHandlerRootView>
             <View>
-                {/* <Pictrue height={150} width={150} ></Pictrue> */}
-                <Message></Message>
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Progress.Bar
+                        progress={0.5}
+                        height={10}
+                        width={200}
+                        color="#1ba035"
+                        borderRadius={5}>
+                    </Progress.Bar>
+                </View>
+
             </View>
         </GestureHandlerRootView>
     )
