0ac2a6a435f65c389ca7a904cf4e75788cccf70d
tanzs <13640282804@163.com>
2025-04-03 17:31:36 +0800

    feat(ReportScreen): 增加表单输入和提交功能

--- src/layouts/ReportScreen.tsx
+++ src/layouts/ReportScreen.tsx
@@ -1,11 +1,54 @@
-import * as React from 'react';
-import { View, Button, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, ScrollView, FlatList } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+
 
 
 
 export default function SecondScreen() {
+    const [formData, setFormData]: any = useState({ field1: '', field2: '', field3: '', field4: '', field5: '', field6: '' })
+
+    const fields = [
+        {
+            titele: "字段1",
+            field: 'field1',
+        },
+        {
+            titele: "字段2",
+            field: 'field2',
+        },
+        {
+            titele: "字段3",
+            field: 'field3',
+        },
+        {
+            titele: "字段4",
+            field: 'field4',
+        },
+        {
+            titele: "字段5",
+            field: 'field5',
+        },
+        {
+            titele: "字段226",
+            field: 'field6',
+        }
+    ]
     return (
-      <Text>123</Text>
+        
+        <View>
+            <ScrollView >
+                {
+                    fields.map((item, index) => {
+                        return <TextInput key={index} label={item.titele} value={formData[item.field]} onChangeText={(text) => setFormData({ ...formData, [item.field]: text })} />;
+                    })
+                }
+            </ScrollView>
+
+            <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+                提交
+            </Button>
+        </View>
     );
 }
 
